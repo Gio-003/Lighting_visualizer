b6ea7a6b57038bd9d7cd861125071a784ed481b5
Gio <ognjen.vujanic03@gmail.com>
2026-04-09 19:39:52 +0200

    Dodat slider za intenzitet svetla. Jos uvek samo testiranja

--- lighting-visualizer/src/main.js
+++ lighting-visualizer/src/main.js
@@ -1,5 +1,6 @@
 import './style.css'
 import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 
 // Get the container
 const canvasContainer = document.querySelector('.canvas-container');
@@ -9,6 +10,9 @@ const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, canvasContainer.clientWidth / canvasContainer.clientHeight, 0.1, 1000);
 camera.position.set(0, 0, 45);
 
+const controls = new OrbitControls(camera, canvasContainer);
+controls.update();
+
 const renderer = new THREE.WebGLRenderer({ 
     canvas: document.getElementById('bg'),
     antialias: true 
@@ -16,17 +20,25 @@ const renderer = new THREE.WebGLRenderer({
 renderer.setSize(canvasContainer.clientWidth, canvasContainer.clientHeight);
 renderer.setPixelRatio(window.devicePixelRatio);
 
-const geometry = new THREE.TorusGeometry(10, 3, 16, 40);
-const material = new THREE.MeshStandardMaterial({ color: 0x0077ff });
+const geometry = new THREE.SphereGeometry(10, 32, 32);
+const material = new THREE.MeshStandardMaterial({ color: 0x0077ff ,wireframe: false});
 const cube = new THREE.Mesh(geometry, material);
 scene.add(cube);
+
 
 const ambientLight = new THREE.AmbientLight(0xffffff, 0.3);
 scene.add(ambientLight);
 
-const pointLight = new THREE.PointLight(0xffffff, 100, 100);
+const pointLight = new THREE.PointLight(0xffffff, 200, 100);
 pointLight.position.set(10, 10, 10);
 scene.add(pointLight);
+
+const lightSlider = document.getElementById('lightSlider');
+lightSlider.addEventListener('input', (event) => {
+    const intensity = event.target.value; // Convert to a value between 0 and 1
+    pointLight.intensity = intensity;
+});
+
 
 // Handle window resizing
 window.addEventListener('resize', () => {
