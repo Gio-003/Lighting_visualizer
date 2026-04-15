e1845d6c88903a9d1bf1b9c0ac4312a8d6a955df
Gio <ognjen.vujanic03@gmail.com>
2026-04-15 16:09:05 +0200

    Uradjen Phong i dodat mogucnost pauze animacije kao neki vid debugera

--- lighting-visualizer/src/main.js
+++ lighting-visualizer/src/main.js
@@ -2,8 +2,8 @@ import './style.css'
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 //import { TeapotGeometry } from 'three/examples/jsm/geometries/TeapotGeometry.js';
-import  vertShader  from './shaders/vertexShader.vert?raw';
-import  fragShader  from './shaders/fragmentShader.frag?raw';
+import  phongVertexShader  from './shaders/phongVertexShader.vert?raw';
+import  phongFragmentShader from './shaders/phongFragmentShader.frag?raw';
 //import bg from './assets/bg.jpg';
 
 // Set up the scene, camera, and renderer and controls
@@ -11,8 +11,6 @@ const canvasContainer = document.querySelector('.canvas-container');
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, canvasContainer.clientWidth / canvasContainer.clientHeight, 0.1, 1000);
 camera.position.set(0, 0, 45);
-// Load and set the background texture CHANGE LATER TO GRADIENT
-//scene.background = new THREE.TextureLoader().load(bg);
 const controls = new OrbitControls(camera, canvasContainer);
 controls.enableDamping = true; // Enable damping for smoother controls
 
@@ -24,20 +22,25 @@ const renderer = new THREE.WebGLRenderer({
 renderer.setSize(canvasContainer.clientWidth, canvasContainer.clientHeight);
 renderer.setPixelRatio(window.devicePixelRatio);
 const pointLight = new THREE.PointLight(0xffffff, 200, 300);
-pointLight.position.set(10, 10, 10);
+pointLight.position.set(200, 70, 100);
 scene.add(pointLight);
+const pointLightHelper = new THREE.PointLightHelper(pointLight, 5);
+scene.add(pointLightHelper);
 // Create a sphere geometry and material, then add it to the scene with a wireframe overlay
 let geometry = new THREE.SphereGeometry(16, 32, 32);
 const material = new THREE.ShaderMaterial({ 
-    vertexShader: vertShader,
-    fragmentShader: fragShader,
+    vertexShader: phongVertexShader,
+    fragmentShader: phongFragmentShader ,
      uniforms: {
         uColor: { value: new THREE.Color(0xa0a0a0) },
         uAmbientStrength : { value: 0.1 },
         uLightColor : { value: new THREE.Color(0xffffff) },
         uLightPos : { value: pointLight.position },
         uDiffuse : { value: 0.7 },
-        uLightStrength : { value: 10.0 }
+        uLightStrength : { value: 1.0 },
+        uSpecularStrength : { value: 0.5 },
+        uViewPos : { value: camera.position },
+        uShininess : { value: 32.0 }
     }
 });
 const cube = new THREE.Mesh(geometry, material);
@@ -52,19 +55,39 @@ wireMesh.visible = false;
 
 //Event listeners for UI controls
 const lightSlider = document.getElementById('lightSlider');
+const lightSSliderValue = document.getElementById('lightSSliderValue');
 lightSlider.addEventListener('input', (event) => {
     const intensity = event.target.value;
     material.uniforms.uLightStrength.value = intensity;
+    lightSSliderValue.textContent = intensity;
 });
 const ambientLightSlider = document.getElementById('ambient');
+const ambientValue = document.getElementById('ambientValue');
 ambientLightSlider.addEventListener('input', (event) => {
     const intensity = event.target.value;
     material.uniforms.uAmbientStrength.value = intensity;
+    ambientValue.textContent = intensity;
 });
 const diffuseSlider = document.getElementById('diffuse');
+const diffuseValue = document.getElementById('diffuseValue');
 diffuseSlider.addEventListener('input', (event) => {
     const intensity = event.target.value;
     material.uniforms.uDiffuse.value = intensity;
+    diffuseValue.textContent = intensity;
+});
+const specularSlider = document.getElementById('specular');
+const specularValue = document.getElementById('specularValue');
+specularSlider.addEventListener('input', (event) => {
+    const intensity = event.target.value;
+    material.uniforms.uSpecularStrength.value = intensity;
+    specularValue.textContent = intensity;
+});
+const shininessSlider = document.getElementById('shininess');
+const shininessValue = document.getElementById('shininessValue');
+shininessSlider.addEventListener('input', (event) => {
+    const shininess = event.target.value;
+    material.uniforms.uShininess.value = shininess;
+    shininessValue.textContent = shininess;
 });
 
 const wireFrameCheckbox = document.getElementById('wireframe');
@@ -101,12 +124,20 @@ window.addEventListener('resize', () => {
     renderer.setSize(canvasContainer.clientWidth, canvasContainer.clientHeight);
     renderer.setPixelRatio(window.devicePixelRatio);
 });
+let isAnimationPaused=false;
 
-
+window.addEventListener('keydown', (event) => {
+    if(event.key=='p'||event.key=='P'){
+        isAnimationPaused=!isAnimationPaused;
+    }
+}); 
 // Animation loop to rotate the cube and render the scene
 function animate() {
     requestAnimationFrame(animate);
-    cube.rotation.y += 0.001;
+    if(!isAnimationPaused) {
+        cube.rotation.y -= 0.001;
+    }
+    material.uniforms.uViewPos.value.copy(camera.position);
     renderer.render(scene, camera);
     controls.update();
 }
--- lighting-visualizer/src/shaders/phongFragmentShader.frag
+++ lighting-visualizer/src/shaders/phongFragmentShader.frag
@@ -3,11 +3,13 @@ precision mediump float;
 uniform float uAmbientStrength; //io*ka ambijentalno svetlo
 uniform float uDiffuse;
 uniform float uLightStrength;
+uniform float uSpecularStrength;
+uniform float uShininess;
 
 uniform vec3 uColor;
 uniform vec3 uLightColor;
 uniform vec3 uLightPos;
-
+uniform vec3 uViewPos;
 
 varying vec3 vNormal;
 varying vec3 vFragPos;
@@ -16,10 +18,17 @@ void main() {
     //Ambijentalno svetlo
     vec3 ambient= uAmbientStrength* uLightColor; //ka *ia
 
+//diffuse
     vec3 norm = normalize(vNormal);
     vec3 lightDir = normalize(uLightPos - vFragPos);
     float diff= max(dot(norm, lightDir), 0.0);
     vec3 diffuse= (uDiffuse*diff * uLightColor);
-    vec3 res= (ambient+ diffuse)*uColor*uLightStrength;
-    gl_FragColor = vec4(res,1.0); // crveno
+//specular
+    vec3 viewDir=normalize(uViewPos-vFragPos);
+    vec3 reflectDir=reflect(-lightDir,norm);
+    float spec = pow(max(dot(viewDir, reflectDir), 0.0), uShininess);
+    vec3 specular=uSpecularStrength*spec*uLightColor;
+
+    vec3 res= ((diffuse+specular)*uLightStrength)+ambient*uColor;
+    gl_FragColor = vec4(res,1.0);
 }
